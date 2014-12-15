47a2231134865f0954cdb40fb0192f57be6c1cce
samstewart78 <samuel@torzo.com>
2014-12-14 18:56:31 -0700

    Inlines for list products and store products

--- listgenerator/admin.py
+++ listgenerator/admin.py
@@ -1,5 +1,9 @@
 from django.contrib import admin
-from listgenerator.models import GroceryList
+from listgenerator.models import GroceryList, ListProducts
+
+class ListProductsInline(admin.TabularInline):
+    model = ListProducts
+    extra = 5
 
 
 class GroceryListAdmin(admin.ModelAdmin):
@@ -8,8 +12,9 @@ class GroceryListAdmin(admin.ModelAdmin):
         ('Totals', {'fields': ['total_estimated', 'total_final']}),
     ]
     list_display = ('name', 'key', 'total_estimated', 'total_final')
-    list_filter = ['date']
     search_fields = ['name', 'key', 'date']
+    list_filter = ['date']
+    inlines = [ListProductsInline]
 
 # Register your models here.
 admin.site.register(GroceryList, GroceryListAdmin)
--- stores/admin.py
+++ stores/admin.py
@@ -1,10 +1,16 @@
 from django.contrib import admin
-from stores.models import Store
+from stores.models import Store, StoreProducts
+
+
+class StoreProductsInline(admin.TabularInline):
+    model = StoreProducts
+    extra = 5
 
 
 class StoreAdmin(admin.ModelAdmin):
     fields = ('name', 'key'),
     search_fields = ['name', 'key']
+    inlines = [StoreProductsInline]
 
 # Register your models here.
 admin.site.register(Store, StoreAdmin)
